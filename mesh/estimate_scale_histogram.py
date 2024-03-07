48c7d8f85e7c406118b075a24063b2e20f2ec3a4
Jonathan Ventura <jventu09@calpoly.edu>
2024-03-07 11:11:15 -0800

    adding script to estimate scale using histogram

---
+++ mesh/estimate_scale_histogram.py
@@ -0,0 +1,39 @@
+import sklearn
+import numpy as np
+import trimesh
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('input',help='path to input mesh')
+parser.add_argument('output',help='output path for scaled mesh')
+parser.add_argument('--bins',default=500,help='number of bins in histogram')
+parser.add_argument('--camera_height',default=2,help='camera height in meters')
+args = parser.parse_args()
+
+print('loading mesh...')
+mesh = trimesh.load(args.input)
+vertices = mesh.vertices
+X = vertices[:, 0]
+Y = vertices[:, 1]
+plane = vertices[:, :2]
+
+print(f'building histogram with {args.bins} bins...')
+hist, bin_edges = np.histogram(Y, bins=args.bins)
+
+print(hist)
+max_bin_index = np.argmax(hist)
+
+Y_max_bin = Y[(Y>bin_edges[max_bin_index])&(Y<=bin_edges[max_bin_index+1])]
+
+cluster_height = np.mean(Y_max_bin)
+
+print(f'height of largest cluster: {cluster_height}')
+
+translated = Y - cluster_height
+
+vertices[:, 1] = translated
+
+mesh.vertices = vertices * 2 / abs(cluster_height)
+
+print('exporting mesh...')
+mesh.export(args.output)
